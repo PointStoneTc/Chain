876cd9d3cd7fef54072f8f52651b858aeb96f065
magicgreek <magicgreek@163.com>
2018-11-13 10:40:17 +0800

    1

--- WhiteWeb_ChainAge/WebContent/static/js/newsContent.js
+++ WhiteWeb_ChainAge/WebContent/static/js/newsContent.js
@@ -1,3 +1,23 @@
+function loadProperties(type) {
+    jQuery.i18n.properties({
+        name: 'strings', // 资源文件名称
+        path: '../i18n/', // 资源文件所在目录路径
+        mode: 'map', // 模式：变量或 Map
+        language: type, // 对应的语言
+        cache: false,
+        encoding: 'UTF-8',
+        callback: function () { // 回调方法
+            //   $('.aa').html($.i18n.prop('string_text'));
+
+        }
+    });
+}
+
+$(document).ready(function () {
+    var LANGUAGE_CODE = jQuery.i18n.normaliseLanguageCode({}); //获取浏览器的语言
+    loadProperties(LANGUAGE_CODE);
+});
+
 var newsContent={
     init:function () {
 
@@ -5,14 +25,14 @@ var newsContent={
         var newsFuc= template($("#news").html(),{model:[{id:1},{id:2},{id:3}]});
         $(".news_con_right .new-list").append(newsFuc);
 
-        var marketFuc= template($("#market").html(),{model:[{id:1},{id:2},{id:3},{id:2},{id:3}]});
-        $(".news_con_right .market-list").append(marketFuc);
-
-        var latestNewsFuc= template($("#latestNews").html(),{model:[{id:1},{id:2},{id:3},{id:2},{id:3}]});
-        $(".news_con_right .latest-news-list").append(latestNewsFuc);
-
-        var exchangeFuc= template($("#exchange").html(),{model:[{id:1},{id:2},{id:3},{id:2},{id:3}]});
-        $(".news_con_right .exchange-list").append(exchangeFuc);
+        // var marketFuc= template($("#market").html(),{model:[{id:1},{id:2},{id:3},{id:2},{id:3}]});
+        // $(".news_con_right .market-list").append(marketFuc);
+        //
+        // var latestNewsFuc= template($("#latestNews").html(),{model:[{id:1},{id:2},{id:3},{id:2},{id:3}]});
+        // $(".news_con_right .latest-news-list").append(latestNewsFuc);
+        //
+        // var exchangeFuc= template($("#exchange").html(),{model:[{id:1},{id:2},{id:3},{id:2},{id:3}]});
+        // $(".news_con_right .exchange-list").append(exchangeFuc);
 
     },
 
