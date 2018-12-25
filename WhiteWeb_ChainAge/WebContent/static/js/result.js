99eb82da475e4b7f60a955d7a337f2d12982c407
zhangyaya <18701428362@139.com>
2018-12-25 17:41:20 +0800

    搜索页面完成

--- WhiteWeb_ChainAge/WebContent/static/js/result.js
+++ WhiteWeb_ChainAge/WebContent/static/js/result.js
@@ -2,6 +2,7 @@ var listNewsPage = {
     pageFlag: 1,
     pageSize: 12,
     advertData: [],
+    total:0,
     init: function () {
         this.eventInit();
         $('.news_mobile_container').attr('num', 0)
@@ -9,7 +10,7 @@ var listNewsPage = {
 
         $(".search_title").text(Common.getQueryString('n'));
         // 请求
-        this.getNewsListData({per_page: 12, order: 'desc', orderby: 'date', categories: 188}, function (param) {
+        this.getNewsListData(1, function (param) {
             listNewsPage.setPagination(param);
         });
 
@@ -28,96 +29,101 @@ var listNewsPage = {
             }
         })
     },
-    getNewsListData: function (param, callback) {
-        // 列表新闻
-        Common.getNewsData(param, function (data) {
-            if ($(".news_mobile_container").is(':hidden')) {//pc端
-                var htm = '';
-                $('.news_list_con .news_container').html('');
-                var categories = param.categories
-                for (var i = 0; i < data.length; i++) {
-                    var linkUrl = 'newsContent.html?id=' + data[i].id + '&cat=' + Common.categories[param.categories];
+    getNewsListData: function (page,callback) {
+        var url = 'http://data.chainage.jp/blockchain/data/post/query?key='+Common.getQueryString('n')+'&page='+page+'&num=10';
+        $.ajax({
+            type: 'GET',
+            url: url,
+            error: function () {
+            },
+            success: function (res) {
+                if (res) {
+                    $.ajax({
+                        type: 'GET',
+                        url: 'http://chainage.cc/wp-json/wp/v2/posts?include='+JSON.parse(res).ids,
+                        error: function () {
+                        },
+                        success: function (data) {
+                            if ($(".news_mobile_container").is(':hidden')) {//pc端
+                                var htm = '';
+                                $('.news_list_con .news_container').html('');
 
-                    if (categories == 147) {
-                        linkUrl = 'chanelContent.html?id=' + data[i].id + '&cat=' + Common.categories[param.categories];
-                    }
-                    listNewsPage.getTagsData(data[i].id, function (json) {
-                        var html = '';
-                        for (var i = 0; i < json.length; i++) {
-                            html += '<span id="' + json[i].id + '">' + json[i].name + '</span>';
+                                for (var i = 0; i < data.length; i++) {
+                                    var linkUrl = 'newsContent.html?id=' + data[i].id ;
+                                   listNewsPage.getTagsData(data[i].id, function (json) {
+                                        var html = '';
+                                        for (var j = 0; j < json.length; j++) {
+                                            html += '<span id="' + json[j].id + '">' + json[j].name + '</span>';
+                                        }
+
+                                       htm = '<div class="search_item" ><a href="' + linkUrl + '">'
+                                           + '<div class="publishDate">' + data[i].date.substr(0, 4) + '年' + data[i].date.substr(5, 2) + '月' + data[i].date.substr(8, 2) + '日</div>'
+                                           + '<div class="search_item_tit">' + data[i].title.rendered + '</div>'
+                                           + '<div class="search_item_des">' + data[i].excerpt.rendered + '</div>'
+                                           + '<div class="tags_name" id="'+data[i].id+'">'+html+'</div>'
+                                           + '</a></div>';
+                                       $('.news_list_con .news_container').append(htm);
+                                       var key = Common.getQueryString('n');
+                                       $(".search_item_tit").html($(".search_item_tit").html().replace(new RegExp(key, 'g'), "<span style='color:red'>" + key + "</span>"));
+
+                                   })
+
+                                }
+                                if (callback) {
+                                    callback(JSON.parse(res).total);
+                                }
+                            }else {
+                                listNewsPage.loadNewsDataData(data,JSON.parse(res).total);
+                            }
+
                         }
 
-                        htm = '<div class="search_item"><a href="' + linkUrl + '">'
-                            + '<div class="publishDate">' + data[i].date.substr(0, 4) + '年' + data[i].date.substr(5, 2) + '月' + data[i].date.substr(8, 2) + '日</div>'
-                            + '<div class="search_item_tit">' + 345667 + '</div>'
-                            + '<div class="search_item_des">' + data[i].excerpt + '</div>'
-                            + '<div class="tags_name">' + html + '</div>'
-                            + '</a></div>';
-                        $('.news_list_con .news_container').append(htm);
-                        var key = Common.getQueryString('n');
-                        $(".search_item_tit").html($(".search_item_tit").html().replace(new RegExp(key, 'g'), "<span style='color:red'>" + key + "</span>"));
                     })
 
-                }
-                if (callback) {
-                    callback(param);
-                }
-            }else {
-                listNewsPage.loadNewsDataData(data,param);
-            }
 
-            listNewsPage.loadNewsDataData(data,param);
+                }
+            }
         });
 
     },
 
     // 加载新闻数据
-    loadNewsDataData: function (data, param) {
+    loadNewsDataData: function (data, total) {
         var more = '<div class="more_btn">加载更多</div>';
         var htm = '';
-        var categories = param.categories;
+
         if (data.length > 0) {
             for (var i = 0; i < data.length; i++) {
-                var linkUrl = 'newsContent.html?id=' + data[i].id + '&cat=' + Common.categories[param.categories];
-
-                if (categories == 147) {
-                    linkUrl = 'chanelContent.html?id=' + data[i].id + '&cat=' + Common.categories[param.categories];
-                }
+                listNewsPage.total++;
+                var linkUrl = 'newsContent.html?id=' + data[i].id ;
                 listNewsPage.getTagsData(data[i].id, function (json) {
                     var html = '';
-                    for (var i = 0; i < json.length; i++) {
-                        html += '<span id="' + json[i].id + '">' + json[i].name + '</span>';
+                    for (var j = 0; j < json.length; j++) {
+                        html += '<span id="' + json[j].id + '">' + json[j].name + '</span>';
                     }
 
-                    htm = '<div class="search_item"><a href="' + linkUrl + '">'
+                    htm = '<div class="search_item" ><a href="' + linkUrl + '">'
                         + '<div class="publishDate">' + data[i].date.substr(0, 4) + '年' + data[i].date.substr(5, 2) + '月' + data[i].date.substr(8, 2) + '日</div>'
-                        + '<div class="search_item_tit">' + 345667 + '</div>'
-                        + '<div class="search_item_des">' + data[i].excerpt + '</div>'
-                        + '<div class="tags_name">' + html + '</div>'
+                        + '<div class="search_item_tit">' + data[i].title.rendered + '</div>'
+                        + '<div class="search_item_des">' + data[i].excerpt.rendered + '</div>'
+                        + '<div class="tags_name" id="'+data[i].id+'">'+html+'</div>'
                         + '</a></div>';
-                    $('.news_list_con .news_container').append(htm);
+                    $(' .news_mobile_container').append(htm);
                     var key = Common.getQueryString('n');
                     $(".search_item_tit").html($(".search_item_tit").html().replace(new RegExp(key, 'g'), "<span style='color:red'>" + key + "</span>"));
-                })
 
+                })
             }
 
             var num = parseInt($('.news_mobile_container').attr('num'));
             //判断是否需要显示加载更多的按钮
-            if (data.length>=10) {
+            if (listNewsPage.total<total) {
                 $('.news_mobile_container ').after(more);
                 $(".more_btn").unbind('click').on('click', function () {
                     $('.more_btn').remove();  //移除加载更多按钮
                     $('.news_mobile_container').attr('num', num + 1);
-                    localStorage.removeItem(param.categories);
-                    listNewsPage.getNewsListData({
-                        per_page: 12,
-                        order: 'desc',
-                        orderby: 'date',
-                        categories: param.categories,
-                        "start": (listNewsPage.pageSize) * num + listNewsPage.pageSize,
-                        "isLoadMore": true
-                    });
+
+                    listNewsPage.getNewsListData($('.news_mobile_container').attr('num'));
                 });
             } else {
                 $('.more_btn').remove(); // 移除加载更多按钮
@@ -125,21 +131,21 @@ var listNewsPage = {
         }
     },
     // 分页显示
-    setPagination: function (param) {
-        $(".page").pagination(Common.newCounts[param.categories], {
+    setPagination: function (total) {
+        $(".page").pagination(total, {
             'items_per_page': 12,
             'current_page': 0,
             'num_display_entries': 6,
             'num_edge_entries': 3,
             'link_to': 'javascript:;',
-            'total': '共' + Common.newCounts[param.categories] + '条',
+            'total': '共' + total + '条',
             'prev_text': "",
             'next_text': "»",
             'call_callback_at_once': false,
             'callback': $.proxy(function (pageIndex, $page) {
-                param = $.extend(param, {page: pageIndex + 1});
-                localStorage.removeItem(param.categories);
-                listNewsPage.getNewsListData(param);
+
+
+                listNewsPage.getNewsListData(pageIndex + 1);
             }, this)
         });
     },
@@ -149,7 +155,7 @@ var listNewsPage = {
         $.ajax({
             type: 'GET',
             url: url,
-            async: true,
+            async: false,
             error: function () {
             },
             success: function (data) {
